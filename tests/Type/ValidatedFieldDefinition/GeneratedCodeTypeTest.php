28a5be46e8e05c4a84021b2f99c6fde56a4328ed
Max Loeb <shmax@shmax.com>
2019-10-06 22:43:56 -0700

    add files

---
+++ tests/Type/ValidatedFieldDefinition/GeneratedCodeTypeTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQL\Tests\Type\ValidatedFieldDefinition;
+
+use GraphQL\GraphQL;
+use GraphQL\Tests\Utils;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ValidatedFieldDefinition;
+use GraphQL\Type\Schema;
+use PHPUnit\Framework\TestCase;
+
+final class GeneratedCodeTypeTest extends TestCase
+{
+    /** @var Type */
+
+    public function testIntCodeType() : void
+    {
+        $schema = new Schema([
+            'query' => new ObjectType(['name' => 'Query']),
+            'mutation' => new ObjectType([
+                'name' => 'Mutation',
+                'fields' => function () {
+                    return [
+                        'updateBook' => new ValidatedFieldDefinition([
+                            'name' => 'updateBook',
+                            'type' => Type::boolean(),
+                            'args' => [
+                                'bookId' => [
+                                    'type' => Type::id(),
+                                    'validate' => function ($bookId) {
+                                        return empty($bookId) ? 1 : 0;
+                                    },
+                                ],
+                            ],
+                            'resolve' => static function ($value) : bool {
+                                return !!$value;
+                            },
+                        ]),
+                    ];
+                },
+            ]),
+        ]);
+
+        $res = GraphQL::executeQuery(
+            $schema,
+            Utils::nowdoc('
+                mutation UpdateBook(
+                    $bookId:ID
+                ) {
+                    updateBook (bookId: $bookId) {
+                        valid
+                        fields {
+                            bookId {
+                                code
+                                msg
+                            }
+                        }
+                        result
+                    }
+                }
+            '),
+            [],
+            null,
+            ['bookId' => null]
+        );
+
+        static::assertEmpty($res->errors);
+        static::assertEquals($res->data['updateBook']['fields']['bookId']['code'], 1);
+    }
+
+    public function testStringCodeType() : void
+    {
+        $schema = new Schema([
+            'query' => new ObjectType(['name' => 'Query']),
+            'mutation' => new ObjectType([
+                'name' => 'Mutation',
+                'fields' => function () {
+                    return [
+                        'updateBook' => new ValidatedFieldDefinition([
+                            'name' => 'updateBook',
+                            'type' => Type::boolean(),
+                            'args' => [
+                                'bookId' => [
+                                    'type' => Type::id(),
+                                    'errorCodes' => [
+                                        'invalidBookId'
+                                    ],
+                                    'validate' => function ($bookId) {
+                                        return empty($bookId) ? ['invalidBookId', "Invalid Book Id"] : 0;
+                                    },
+                                ],
+                            ],
+                            'resolve' => static function ($value) : bool {
+                                return !!$value;
+                            },
+                        ]),
+                    ];
+                },
+            ]),
+        ]);
+
+        $res = GraphQL::executeQuery(
+            $schema,
+            Utils::nowdoc('
+                mutation UpdateBook(
+                    $bookId:ID
+                ) {
+                    updateBook (bookId: $bookId) {
+                        valid
+                        fields {
+                            bookId {
+                                code
+                                msg
+                            }
+                        }
+                        result
+                    }
+                }
+            '),
+            [],
+            null,
+            ['bookId' => null]
+        );
+
+        static::assertEmpty($res->errors);
+        static::assertEquals($res->data['updateBook']['fields']['bookId']['code'], "invalidBookId");
+        static::assertEquals($res->data['updateBook']['fields']['bookId']['msg'], "Invalid Book Id");
+    }
+}
